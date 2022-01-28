4379f5dbd1757bd148989120bb21a325a46218c8
renan.modesto <renan.modesto@grupomultiplica.com>
2022-01-28 13:46:45 -0300

    supabase no projeto

--- pages/chat.js
+++ pages/chat.js
@@ -1,34 +1,28 @@
 import { Box, Text, TextField, Image, Button } from "@skynexui/components";
-
 import React from "react";
-
 import appConfig from "../config.json";
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM4MDc1MiwiZXhwIjoxOTU4OTU2NzUyfQ.g5PH9ht9hMCgBoKOtJKqgriulZs4AA1Do2S-77G2ycU";
+
+const SUPABASE_URL = "";
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default function ChatPage() {
-  const [mensagem, setMensagem] = React.useState("");
-
-  const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+  const [mensagem, setMensagem] = useState("");
+  const [listaDeMensagens, setListaDeMensagens] = useState([]);
 
   /*
-
     // Usuário
-
     - Usuário digita no campo textarea
-
     - Aperta enter para enviar
-
     - Tem que adicionar o texto na listagem
-
-    
-
+   
     // Dev
-
     - [X] Campo criado
-
     - [X] Vamos usar o onChange usa o useState (ter if pra caso seja enter pra limpar a variavel)
-
     - [X] Lista de mensagens 
-
     */
 
   function handleNovaMensagem(novaMensagem) {
@@ -48,38 +42,25 @@ export default function ChatPage() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-
-        backgroundColor: appConfig.theme.colors.primary[500],
-
-        backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
-
+        backgroundColor: appConfig.theme.colors.primary[200],
+        backgroundImage: `url(https://starwarsblog.starwars.com/wp-content/uploads/2020/04/star-wars-backgrounds-27.jpg)`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundBlendMode: "multiply",
-
         color: appConfig.theme.colors.neutrals["000"],
       }}
     >
       <Box
         styleSheet={{
           display: "flex",
-
           flexDirection: "column",
-
           flex: 1,
-
           boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
-
           borderRadius: "5px",
-
           backgroundColor: appConfig.theme.colors.neutrals[700],
-
           height: "100%",
-
           maxWidth: "95%",
-
           maxHeight: "95vh",
-
           padding: "32px",
         }}
       >
@@ -213,16 +194,12 @@ function MessageList(props) {
               <Image
                 styleSheet={{
                   width: "20px",
-
                   height: "20px",
-
                   borderRadius: "50%",
-
                   display: "inline-block",
-
                   marginRight: "8px",
                 }}
-                src={`https://github.com/vanessametonini.png`}
+                src={`https://github.com/rednand.png`}
               />
 
               <Text tag="strong">{mensagem.de}</Text>
@@ -230,9 +207,7 @@ function MessageList(props) {
               <Text
                 styleSheet={{
                   fontSize: "10px",
-
                   marginLeft: "8px",
-
                   color: appConfig.theme.colors.neutrals[300],
                 }}
                 tag="span"
@@ -240,7 +215,6 @@ function MessageList(props) {
                 {new Date().toLocaleDateString()}
               </Text>
             </Box>
-
             {mensagem.texto}
           </Text>
         );
